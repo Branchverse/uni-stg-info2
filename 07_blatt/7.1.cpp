0114de3bb7ae0c62a1d679abddf608d0b64667e0
Branchverse <maximilian.dolbaum@gmail.com>
2023-08-21 00:04:50 +0200

    add: comments

--- 07_blatt/7.1.cpp
+++ 07_blatt/7.1.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void towerOfHanoi(int n, char start, char end, char middle)
-{
+int counter = 1;
+void towerOfHanoi(int n, char start, char end, char help)
+{ // Comment for n=3
   if (n == 1)
   {
-    cout << start << "=>" << end << endl;
+    // cout << "Move top piece to end" << endl;
+    cout << start << "=>" << end << endl; // 1. 3. 5. 7.
     return;
   }
-  towerOfHanoi(n - 1, start, middle, end);
-  cout << start << "=>" << end << endl;
-  towerOfHanoi(n - 1, middle, end, start);
+
+  // cout << "Move Tower size " << n - 1 << " to helper" << endl;
+  towerOfHanoi(n - 1, start, help, end);
+  // cout << "Move last piece to end" << endl;
+  cout << start << "=>" << end << endl; // 2. 4. 6.
+  // cout << "Move Tower size " << n - 1 << " from helper to End" << endl;
+  towerOfHanoi(n - 1, help, end, start);
 }
 
 int main()
 {
   towerOfHanoi(2, 'A', 'C', 'B');
+  counter = 1;
   cout << endl;
   towerOfHanoi(3, 'A', 'C', 'B');
+  counter = 1;
   cout << endl;
   towerOfHanoi(4, 'A', 'C', 'B');
 }
