e773d3e602913b0a000135c6a5f78a89fdf4a44e
Branchverse <maximilian.dolbaum@gmail.com>
2023-08-23 17:07:59 +0200

    ref: 5.2

--- 05_blatt/5.2.cpp
+++ 05_blatt/5.2.cpp
@@ -6,13 +6,9 @@ void print_triangle(int n)
   for (int i = 0; i < n; i++)
   {
     for (int j = 1; j < n - i; j++)
-    {
       cout << " ";
-    }
     for (int j = 0; j < i + 1; j++)
-    {
       cout << "X ";
-    }
     cout << endl;
   }
   cout << endl;
